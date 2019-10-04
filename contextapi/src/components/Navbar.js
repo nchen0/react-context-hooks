fe68e9ba02841fe8136916befc1240ba0d5efa88
nchen0 <nicky.chen0@gmail.com>
2019-10-04 16:09:07 -0400

    updating context

--- contextapi/src/components/Navbar.js
+++ contextapi/src/components/Navbar.js
@@ -2,21 +2,24 @@ import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends Component {
-  // This looks up the component tree and find the first time it finds a themecontextprovider and it will have access to the data that the contextprovider is providing.
-  static contextType = ThemeContext;
   render() {
-    // This.context is now the data that we are passing through in that provider.
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
