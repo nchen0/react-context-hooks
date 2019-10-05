bf7c37eee69b9fe3ed3247fdc593d086fb3eef1c
nchen0 <nicky.chen0@gmail.com>
2019-10-05 16:00:24 -0400

    add reducers

---
+++ booklist/src/components/BookForm.js
@@ -0,0 +1,35 @@
+import React, { useContext, useState } from "react";
+import { BookContext } from "../contexts/BookContext";
+
+const BookForm = () => {
+  const { dispatch } = useContext(BookContext);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: "ADD_BOOK", book: { title, author } });
+    setTitle("");
+    setAuthor("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={title}
+        type="text"
+        placeholder="Book Title"
+        onChange={e => setTitle(e.target.value)}
+        required
+      />
+      <input
+        value={author}
+        type="text"
+        placeholder="Book Author"
+        onChange={e => setAuthor(e.target.value)}
+        required
+      />
+      <input type="submit" value="Add Book" />
+    </form>
+  );
+};
+
+export default BookForm;
--- booklist/src/contexts/BookContext.js
+++ booklist/src/contexts/BookContext.js
@@ -1,23 +1,12 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useReducer } from "react";
+import { bookReducer } from "../reducers/bookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
-  const [books, setBooks] = useState([
-    { title: "The Name of the Wind", author: "Patrick Rothfuss", id: 1 },
-    { title: "The Final Empire", author: "Brandon Sanderson", id: 2 }
-  ]);
-  const addBook = (title, author) => {
-    setBooks([...books, { title, author, id: books.length + 1 }]);
-  };
-  const removeBook = id => {
-    setBooks(books.filter(book => book.id !== id));
-  };
-  return (
-    <BookContext.Provider value={{ books, addBook, removeBook }}>
-      {props.children}
-    </BookContext.Provider>
-  );
+  // First item is the reducer, second is the initial state.
+  const [books, dispatch] = useReducer(bookReducer, []);
+  return <BookContext.Provider value={{ books, dispatch }}>{props.children}</BookContext.Provider>;
 };
 
 export default BookContextProvider;
